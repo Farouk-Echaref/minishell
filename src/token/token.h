3fc001bbe2f83d0941ac8dbceb829c01a98c4409
mzarhou <mzarhou.cc@gmail.com>
2022-06-22 00:58:15 +0100

    chore: enumarte e_type

--- src/token/token.h
+++ src/token/token.h
@@ -9,34 +9,33 @@ typedef struct s_token t_token;
 
 enum    e_type
 {
-    COMMAND,
-	CMD_OPTION,
-    CMD_ARG,
-	REDIR_RIGHT ,
-	REDIR_RIGHT ,
-	REDIR_LEFT ,
-	SHIFT_LEFT,
-	SHIFT_RIGHT,
-	AND_OPR,
-	OR_OPR,
-	STAR,
-	LEFT_PAR,
-	RIGHT_PAR,
-	SING_QUOT,
-	DOUB_QUOT,
-	PIPE,
-    FILE_NAME,
-    EXPRESSION,
-    OTHER
+    COMMAND = 1,
+	CMD_OPTION = 2,
+    CMD_ARG = 3,
+	REDIR_RIGHT = 4,
+	REDIR_LEFT = 5,
+	SHIFT_LEFT = 6,
+	SHIFT_RIGHT = 7,
+	AND_OPR = 8,
+	OR_OPR = 9,
+	STAR = 10,
+	LEFT_PAR = 11,
+	RIGHT_PAR = 12,
+	SING_QUOT = 13,
+	DOUB_QUOT = 14,
+	PIPE = 15,
+    FILE_NAME = 16,
+    EXPRESSION = 17,
+    OTHER = 18
 };
 
 struct s_token
 {
-    t_type  type;
-    char    *value;
-    int     length;
+    t_type		type;
+    const char	*value;
+    int			length;
 };
 
-t_token *ft_new_token(const char *value, t_type *type, int length);
+t_token *ft_new_token(const char *value, t_type type, int length);
 
 #endif
