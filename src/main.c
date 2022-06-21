ef70922c668ca72af304afc364961a4a3751a033
mzarhou <mzarhou.cc@gmail.com>
2022-06-21 21:13:15 +0100

    chore: add .editorconfig

--- src/main.c
+++ src/main.c
@@ -6,38 +6,38 @@
 /*   By: mzarhou <mzarhou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 02:16:11 by mzarhou           #+#    #+#             */
-/*   Updated: 2022/06/18 03:34:17 by mzarhou          ###   ########.fr       */
+/*   Updated: 2022/06/20 21:02:27 by mzarhou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft/libft.h"
 
-typedef enum e_type t_type;
-typedef struct s_token t_token;
+typedef enum e_type	t_type;
+typedef struct s_token	t_token;
 
-enum    e_type
+enum	e_type
 {
-    e_operator = 1,
-    e_command
+	e_operator = 1,
+	e_command
 };
 
-struct s_token
+struct	s_token
 {
-    t_type  type;
-    char    *value;
-    int     length;
+	t_type	type;
+	char	*value;
+	int		length;
 };
 
 
-t_list  *ft_tokonize(char *s)
+t_list	*ft_tokonize(char *s)
 {
-    (void)s;
-    return (NULL);
+	(void)s;
+	return (NULL);
 }
 
 int main()
 {
-    char    *s = "ls -a > test.txt && cat << ts 1> test.txt";
-    printf("%p\n", ft_tokonize(s));
+	char	*s = "ls -a > test.txt && cat << ts 1> test.txt";
+	printf("%p\n", ft_tokonize(s));
 }
