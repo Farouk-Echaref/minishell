925a1ec63e3342c919ef8324ce32202ba59b262b
mzarhou <mzarhou.cc@gmail.com>
2022-06-27 16:18:49 +0100

    feat: print tree with types

--- src/utils/print_tree.c
+++ src/utils/print_tree.c
@@ -4,6 +4,52 @@
 #include "token/token.h"
 
 typedef struct Trunk Trunk;
+
+char	*ft_get_type_name(t_type t)
+{
+	switch (t)
+	{
+		case COMMAND:
+			return "CMD";
+		case CMD_OPTION:
+			return "CMD_OPT";
+		case CMD_ARG:
+			return "CMD_ARG";
+		case WHITE_SPACE:
+			return "SPACE";
+		case REDIR_RIGHT:
+			return "REDR";
+		case REDIR_LEFT:
+			return "REDL";
+		case SHIFT_LEFT:
+			return "SHL";
+		case SHIFT_RIGHT:
+			return "SHR";
+		case AND_OPR:
+			return "AND";
+		case OR_OPR:
+			return "OR";
+		case STAR:
+			return "STAR";
+		case SUB_CMD:
+			return "SUB_CMD";
+		case SING_QUOT:
+			return "SING_Q";
+		case DOUB_QUOT:
+			return "DOUB_Q";
+		case PIPE:
+			return "PIPE";
+		case VAR:
+			return "VAR";
+		case FILE_NAME:
+			return "FILE_NAME";
+		case EXPRESSION:
+			return "EXPR";
+		default:
+			return "OTHER";
+	}
+}
+
 struct Trunk
 {
     Trunk	*prev;
@@ -53,7 +99,7 @@ static void printTreeRec(t_tree* root, Trunk *prev, int isLeft)
 
     showTrunks(trunk);
 	t_token	*token = root->content;
-	printf(" %s\n", ft_str(token->value, token->length));
+	printf(" %s(%s)\n", ft_str(token->value, token->length), ft_get_type_name(token->type));
 
     if (prev) {
         prev->str = prev_str;
