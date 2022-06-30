21de464fec08246e7839a0f44d01140772dda7b6
mzarhou <mzarhou.cc@gmail.com>
2022-07-01 00:52:48 +0100

    ft_move_redirections.c

--- src/correction/ft_move_redirections.c
+++ src/correction/ft_move_redirections.c
@@ -6,7 +6,7 @@
 /*   By: mzarhou <mzarhou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 02:05:37 by mzarhou           #+#    #+#             */
-/*   Updated: 2022/06/30 06:27:18 by mzarhou          ###   ########.fr       */
+/*   Updated: 2022/07/01 00:52:18 by mzarhou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ t_list	*ft_get_target(t_list *current)
 	t_list	*tokens;
 
 	if (! current)
-		return NULL;
+		return (NULL);
 	tokens = current;
 	while (current)
 	{
@@ -40,12 +40,12 @@ t_list	*ft_get_target(t_list *current)
 			|| ft_get_token_type(current) == OR_OPR
 			|| ft_get_token_type(current) == PIPE
 		)
-			break;
+			break ;
 		current = current->next;
 	}
 	if (current)
-		return current->prev;
-	return ft_lstlast(tokens);
+		return (current->prev);
+	return (ft_lstlast(tokens));
 }
 
 t_list	*ft_get_list_head(t_list *current)
@@ -55,8 +55,14 @@ t_list	*ft_get_list_head(t_list *current)
 	head = current;
 	while (current)
 	{
-		if (ft_get_token(current)->type == EXPRESSION && (current->prev == NULL || ! ft_is_redirection(ft_get_token(current->prev)->type)))
-			break;
+		if (
+			ft_get_token(current)->type == EXPRESSION
+			&& (
+				current->prev == NULL
+				|| ! ft_is_redirection(ft_get_token(current->prev)->type)
+			)
+		)
+			break ;
 		current = current->next;
 	}
 	if (current)
@@ -74,10 +80,13 @@ t_list	*ft_move_redirections(t_list *current)
 	head = ft_get_list_head(current);
 	while (current)
 	{
-		if (! (ft_is_redirection(ft_get_token_type(current)) && ft_get_token_type(current->next) == EXPRESSION))
+		if (! (
+				ft_is_redirection(ft_get_token_type(current))
+				&& ft_get_token_type(current->next) == EXPRESSION
+			))
 		{
 			current = current->next;
-			continue;
+			continue ;
 		}
 		target = ft_get_target(current);
 		redir_token = current;
