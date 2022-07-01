fbe9b084d2b8e0f140197e6ea0016c7b71102064
mzarhou <mzarhou.cc@gmail.com>
2022-07-01 02:46:07 +0100

    norm correction

--- src/correction/ft_move_redirections.c
+++ src/correction/ft_move_redirections.c
@@ -6,25 +6,13 @@
 /*   By: mzarhou <mzarhou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 02:05:37 by mzarhou           #+#    #+#             */
-/*   Updated: 2022/07/01 00:52:18 by mzarhou          ###   ########.fr       */
+/*   Updated: 2022/07/01 01:05:19 by mzarhou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "correction.h"
 #include <stdio.h>
 #include "utils/utils.h"
-
-int	ft_is_redirection(t_type token_type)
-{
-	if (
-		token_type == REDIR_LEFT
-		|| token_type == REDIR_RIGHT
-		|| token_type == SHIFT_LEFT
-		|| token_type == SHIFT_RIGHT
-	)
-		return (1);
-	return (0);
-}
 
 t_list	*ft_get_target(t_list *current)
 {
