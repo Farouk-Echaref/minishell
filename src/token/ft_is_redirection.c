c7d76ab649d51aab67f379ef7636d89c193c0bfc
mzarhou <mzarhou.cc@gmail.com>
2022-07-01 02:45:33 +0100

    norm token

--- src/token/ft_is_redirection.c
+++ src/token/ft_is_redirection.c
@@ -1,24 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_new_token.c                                     :+:      :+:    :+:   */
+/*   ft_is_redirection.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mzarhou <mzarhou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/22 05:27:18 by fech-cha          #+#    #+#             */
-/*   Updated: 2022/07/01 00:57:12 by mzarhou          ###   ########.fr       */
+/*   Created: 2022/07/01 01:05:26 by mzarhou           #+#    #+#             */
+/*   Updated: 2022/07/01 01:05:45 by mzarhou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "token.h"
+#include "token/token.h"
 
-t_token	*ft_new_token(const char *value, t_type type, int length)
+int	ft_is_redirection(t_type token_type)
 {
-	t_token	*t;
-
-	t = malloc(sizeof(t_token));
-	t->length = length;
-	t->type = type;
-	t->value = value;
-	return (t);
+	if (
+		token_type == REDIR_LEFT
+		|| token_type == REDIR_RIGHT
+		|| token_type == SHIFT_LEFT
+		|| token_type == SHIFT_RIGHT
+	)
+		return (1);
+	return (0);
 }
