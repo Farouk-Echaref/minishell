cbcadd41ed9f4b65266b2bb5379fcf1017032ad8
mzarhou <mzarhou.cc@gmail.com>
2022-07-02 05:13:55 +0100

    transform single/double qoutes -> expression
    fix #10

--- src/lexer/ft_move2_next_token.c
+++ src/lexer/ft_move2_next_token.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_move2next_token.c                               :+:      :+:    :+:   */
+/*   ft_move2_next_token.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mzarhou <mzarhou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 04:34:09 by fech-cha          #+#    #+#             */
-/*   Updated: 2022/07/01 04:34:32 by fech-cha         ###   ########.fr       */
+/*   Updated: 2022/07/02 01:37:10 by mzarhou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@ void	ft_move2_next_token(t_lexer *lxr)
 {
 	while (lxr->content
 		&& *lxr->content
-		&& ft_get_type_of_char(*lxr->content) == OTHER
+		&& ft_get_type_of_char(*lxr->content) != WHITE_SPACE
+		&& ft_get_type_of_char(*lxr->content) != AND_OPR
+		&& ft_get_type_of_char(*lxr->content) != OR_OPR
+		&& ft_get_type_of_char(*lxr->content) != PIPE
 	)
 		lxr->content++;
 }
