868c7b208dc46fbb089154b9f37228c5613f9e8a
mzarhou <mzarhou.cc@gmail.com>
2022-07-21 21:22:47 +0000

    chore: arr_push helper

--- src/arr_utils/ft_arr_push.c
+++ src/arr_utils/ft_arr_push.c
@@ -1,24 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   arr_utils.h                                        :+:      :+:    :+:   */
+/*   ft_arr_push.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mzarhou <mzarhou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/02 07:04:48 by mzarhou           #+#    #+#             */
-/*   Updated: 2022/07/21 21:14:26 by mzarhou          ###   ########.fr       */
+/*   Created: 2022/07/21 21:11:57 by mzarhou           #+#    #+#             */
+/*   Updated: 2022/07/21 21:13:52 by mzarhou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ARR_UTILS_H
-# define ARR_UTILS_H
+#include "arr_utils.h"
 
-# include <stdlib.h>
-# include "utils/utils.h"
+char	**ft_arr_push(char **command, char *str)
+{
+	char	**new_arr;
+	int		new_size;
+	int		i;
 
-void	ft_reverse_arr(char **arr);
-int		ft_arr_size(char **arr);
-char	**ft_arr_shift(char **command, char *str);
-char	**ft_arr_push(char **command, char *str);
-
-#endif
+	new_size = ft_arr_size(command) + 2;
+	new_arr = (char **)malloc(sizeof(char *) * new_size);
+	i = 0;
+	while (command[i])
+	{
+		new_arr[i] = command[i];
+		i++;
+	}
+	new_arr[i] = str;
+	command = ft_free(command);
+	new_arr[i + 1] = NULL;
+	return (new_arr);
+}
