98e918561d35ec381656315477b5e9bd678a41b5
mzarhou <mzarhou.cc@gmail.com>
2022-07-22 16:24:40 +0100

    feat: command path

--- src/evaluator/ft_execute.c
+++ src/evaluator/ft_execute.c
@@ -6,11 +6,44 @@
 /*   By: mzarhou <mzarhou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 11:30:01 by mzarhou           #+#    #+#             */
-/*   Updated: 2022/07/06 14:02:26 by mzarhou          ###   ########.fr       */
+/*   Updated: 2022/07/22 16:22:58 by mzarhou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "evaluator.h"
+
+static char *ft_join_command_path(char *path, char *command_name)
+{
+    char    *command_path;
+    char    *temp;
+
+    temp = ft_strjoin(path, "/");
+    command_path = ft_strjoin(temp, command_name);
+    temp = ft_free(temp);
+    return (command_path);
+}
+
+static	char *ft_get_command_path(char	*command_name, char	**env)
+{
+	char	*path_env;
+	char	**paths;
+    char    *command_path;
+    int     i;
+
+	(void)command_name;
+	path_env = ft_evaluate_var("PATH", env);
+	paths = ft_split(path_env, ':');
+    i = 0;
+	while (paths && paths[i])
+	{
+        command_path = ft_join_command_path(paths[i], command_name);
+        if (access(command_path, X_OK) == 0)
+            return (ft_arr_free(paths), command_path);
+        command_path = ft_free(command_path);
+		i++;
+	}
+	return (ft_arr_free(paths), NULL);
+}
 
 void	ft_execute(t_evaluator_data *evaluator_data, char **argenv)
 {
@@ -38,7 +71,14 @@ void	ft_execute(t_evaluator_data *evaluator_data, char **argenv)
 			dup2(evaluator_data->redirect_left, STDIN_FILENO);
 			close(evaluator_data->redirect_left);
 		}
-		path = ft_strjoin("/bin/", command[0]);
+		path = ft_get_command_path(command[0], argenv);
+		if (! path)
+		{
+			ft_putstr_fd("minishell: ", 1);
+			ft_putstr_fd(command[0], 1);
+			ft_putstr_fd(": command not found\n", 1);
+			exit(1);
+		}
 		execve(path, command, argenv);
 	}
 	else
