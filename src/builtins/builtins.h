944c19c0d8cdfd74bdcb92b91fca4d7ded36d201
mzarhou <mzarhou.cc@gmail.com>
2022-07-23 18:02:21 +0100

    chore: builtin commands init

---
+++ src/builtins/builtins.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzarhou <mzarhou@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/23 16:56:35 by mzarhou           #+#    #+#             */
+/*   Updated: 2022/07/23 17:59:51 by mzarhou          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUILTINS_H
+# define BUILTINS_H
+
+# include "str_utils/str_utils.h"
+
+typedef void (*t_command_func)(char **);
+
+int				ft_is_builtin(char *command_name);
+t_command_func	ft_select_builtin_command(char *command_name);
+
+#endif
