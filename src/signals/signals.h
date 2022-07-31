667bada3ae6af650dec5e3838dd812b0b8ce5fae
mzarhou <mzarhou.cc@gmail.com>
2022-08-01 00:57:36 +0100

    chore: norm signals

--- src/signals/signals.h
+++ src/signals/signals.h
@@ -6,21 +6,21 @@
 /*   By: mzarhou <mzarhou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 19:52:26 by mzarhou           #+#    #+#             */
-/*   Updated: 2022/07/28 15:22:55 by mzarhou          ###   ########.fr       */
+/*   Updated: 2022/08/01 00:57:16 by mzarhou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SIGNALS_H
 # define SIGNALS_H
 
-#include "termios.h"
-#include "data/data.h"
+# include "termios.h"
+# include "data/data.h"
 
 void	setup_term(void);
-void	ft_ctrl_d();
+void	ft_ctrl_d(void);
 void	ft_ctrl_c(int sig);
-void	ft_handle_signals();
+void	ft_handle_signals(void);
 void	ft_setup_term(void);
-void	ft_set_signals_default();
+void	ft_set_signals_default(void);
 
 #endif
