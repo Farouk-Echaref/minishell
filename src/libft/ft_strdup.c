c3a09979e8996147458e41051fafffed9b0489ba
mzarhou <mzarhou.cc@gmail.com>
2022-08-01 00:19:00 +0100

    chore: norm libft

--- src/libft/ft_strdup.c
+++ src/libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: mzarhou <mzarhou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 13:41:15 by mzarhou           #+#    #+#             */
-/*   Updated: 2022/07/05 11:53:48 by mzarhou          ###   ########.fr       */
+/*   Updated: 2022/08/01 00:18:41 by mzarhou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strdup(const char *s1)
 	int		i;
 
 	if (! s1)
-		return ft_strdup("");
+		return (ft_strdup(""));
 	len = ft_strlen(s1);
 	res = (char *)malloc(len + 1);
 	if (!res)
