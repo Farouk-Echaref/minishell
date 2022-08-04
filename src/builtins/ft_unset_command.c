a9caa832d88ba9bbc2ca872e5a6e444584dcaf40
mzarhou <mzarhou.cc@gmail.com>
2022-08-04 02:05:56 +0100

    fix: unset cmd

--- src/builtins/ft_unset_command.c
+++ src/builtins/ft_unset_command.c
@@ -6,7 +6,7 @@
 /*   By: mzarhou <mzarhou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 14:24:21 by mzarhou           #+#    #+#             */
-/*   Updated: 2022/07/31 22:11:17 by mzarhou          ###   ########.fr       */
+/*   Updated: 2022/08/04 01:59:36 by mzarhou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,13 @@ static int	ft_is_target(char *var_name, char *env_line)
 	return (0);
 }
 
-void	ft_unset_command(char **command)
+static void	ft_unset(char *var_name)
 {
-	char	*var_name;
 	t_list	*env;
 
-	var_name = command[1];
 	env = g_.env;
+	if (! var_name)
+		return ;
 	if (ft_is_target(var_name, env->content))
 		g_.env = env->next;
 	while (env)
@@ -44,5 +44,23 @@ void	ft_unset_command(char **command)
 			break ;
 		}
 		env = env->next;
+	}
+}
+
+void	ft_unset_command(char **argv)
+{
+	int		i;
+
+	i = 1;
+	while (argv[i])
+	{
+		if (! ft_check_var_syntax(argv[i]))
+		{
+			ft_error_message(argv[i], "not a valid identifier");
+			g_.exit_status = 1;
+		}
+		else
+			ft_unset(argv[i]);
+		i++;
 	}
 }
